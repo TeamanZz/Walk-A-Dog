e34354dc240a78435f25265c363d0814390e2f2f
Alexey <a.yasinko@mail.ru>
2022-09-16 01:03:37 +0300

    Physics and animations improvements

--- Assets/Scripts/DogsController.cs
+++ Assets/Scripts/DogsController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using DG.Tweening;
 
 public class DogsController : MonoBehaviour
 {
@@ -12,31 +13,47 @@ public class DogsController : MonoBehaviour
     public Transform targetDog;
     public Transform navigationTarget;
 
+    private bool joystickWasDisabled;
+
+    private float movementSpeed;
+
     private void Update()
     {
+        //On Stick Release (1 time)
+        if (joystickHandle.activeSelf == false && !joystickWasDisabled)
+        {
+            for (int i = 0; i < dogsAnimators.Count; i++)
+            {
+                movementSpeed = dogsAnimators[i].GetFloat("Movement_f");
+                DOTween.To(() => movementSpeed, x => movementSpeed = x, 0, 0.5f);
+                dogsAgents[i].isStopped = true;
+                joystickWasDisabled = true;
+            }
+        }
+        //On Stick hold
+        else
+        {
+            navigationTarget.localPosition = targetDog.position + new Vector3(joystick.Horizontal * 15, 0, joystick.Vertical * 15);
+
+            for (int i = 0; i < dogsAnimators.Count; i++)
+            {
+                float joystickValue = Mathf.Abs(joystick.Horizontal) + Mathf.Abs(joystick.Vertical);
+                dogsAnimators[i].SetFloat("Movement_f", joystickValue);
+                dogsAgents[i].isStopped = false;
+                dogsAgents[i].speed = joystickValue * 10;
+                dogsAgents[i].destination = navigationTarget.position;
+            }
+            joystickWasDisabled = false;
+        }
+
+        //On Stick disabled
         if (joystickHandle.activeSelf == false)
         {
             for (int i = 0; i < dogsAnimators.Count; i++)
             {
-                if (dogsAgents[i].gameObject.activeSelf == true)
-                {
-                    dogsAnimators[i].SetFloat("Movement_f", (0));
-                    dogsAgents[i].isStopped = true;
-                }
-            }
-            return;
-        }
-
-        navigationTarget.localPosition = targetDog.position + new Vector3(joystick.Horizontal * 15, 0, joystick.Vertical * 15);
-
-        for (int i = 0; i < dogsAnimators.Count; i++)
-        {
-            if (dogsAgents[i].gameObject.activeSelf == true)
-            {
-                dogsAnimators[i].SetFloat("Movement_f", (1));
-                dogsAgents[i].isStopped = false;
-                dogsAgents[i].destination = navigationTarget.position;
+                dogsAnimators[i].SetFloat("Movement_f", (movementSpeed));
             }
         }
+
     }
 }
