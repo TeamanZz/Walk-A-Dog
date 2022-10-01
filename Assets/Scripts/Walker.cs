78c1ef84800c6273cd80f681c69ef2bad6985e4e
Alexey <a.yasinko@mail.ru>
2022-10-01 23:46:32 +0300

    Walkers Fall

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Walker : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Vector2 targetXAllowedValues;
+    public Vector2 targetYAllowedValues;
+
+    public Animator animator;
+
+    public List<Rigidbody> rbs = new List<Rigidbody>();
+    public List<Collider> colliders = new List<Collider>();
+    private DogBase dog;
+
+    public CapsuleCollider detectionCollider;
+
+    public Rigidbody rb;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        foreach (var item in colliders)
+        {
+            item.enabled = false;
+        }
+
+        foreach (var item in rbs)
+        {
+            item.isKinematic = true;
+        }
+    }
+
+    private void Start()
+    {
+        Vector3 targetPos = new Vector3(Random.Range(targetXAllowedValues.x, targetXAllowedValues.y), transform.position.y, Random.Range(targetYAllowedValues.x, targetYAllowedValues.y));
+        agent.destination = targetPos;
+        StartCoroutine(SetNewDestination());
+    }
+
+    private IEnumerator SetNewDestination()
+    {
+        yield return new WaitForSeconds(Random.Range(8, 20));
+        Vector3 targetPos = new Vector3(Random.Range(targetXAllowedValues.x, targetXAllowedValues.y), transform.position.y, Random.Range(targetYAllowedValues.x, targetYAllowedValues.y));
+        agent.destination = targetPos;
+        yield return SetNewDestination();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.TryGetComponent<DogBase>(out dog))
+        {
+            detectionCollider.enabled = false;
+            agent.enabled = false;
+            foreach (var item in rbs)
+            {
+                item.isKinematic = false;
+            }
+
+            foreach (var item in colliders)
+            {
+                item.enabled = true;
+            }
+
+            animator.enabled = false;
+            foreach (var item in rbs)
+            {
+                item.AddForce(Vector3.up * 100, ForceMode.Impulse);
+            }
+        }
+    }
+}
